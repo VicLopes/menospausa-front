27f1967b71cd414bfccc4a5855790d130acd26c7
Victor Lopes <viclopes2621@gmail.com>
2024-12-17 15:42:36 -0300

    feat: adds form component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,13 +3,23 @@ import { StyledButton } from "./Button.styles";
 
 export type ButtonProps = {
   label: string;
-  onClick: () => void;
+  onClick: (args: unknown) => void;
   primary: boolean;
+  buttonType?: "button" | "submit" | "reset";
 };
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, primary }) => {
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  primary,
+  buttonType,
+}) => {
   return (
-    <StyledButton type="button" onClick={onClick} primary={primary}>
+    <StyledButton
+      type={buttonType ? buttonType : "button"}
+      onClick={onClick}
+      primary={primary}
+    >
       {label}
     </StyledButton>
   );
